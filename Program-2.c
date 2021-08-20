2e820bec809ede58c72f3729923b801a66f3656a
hemihemith <84697170+hemihemith@users.noreply.github.com>
2021-08-20 22:17:51 +0530

    Update Program-2.c

--- Program-2.c
+++ Program-2.c
@@ -1,7 +1,7 @@
 // Program to add information of student and display marks
-//Input: 10 20 30
-//Output: 10 20 30
 
+//Input: 10 20 30
+ //Output: 10 20 30
 #include<stdio.h>  
 struct student  
 {  //char name[10];
@@ -13,9 +13,10 @@ int main()
   struct student s1,s2,s3;  
   printf("Enter the  marks of student 1,2 and 3"); 
   scanf("%d%d%d",&s1.marks,&s2.marks,&s3.marks); 
-
+// Read the marks of three students
  
    printf("%d %d %d",s1.marks,s2.marks,s3.marks); 
   return 0;
 } 
 
+//DONE
